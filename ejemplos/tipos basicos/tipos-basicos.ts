2fe4cfd9ae3bf043d323f61d60d87e14f052ece2
Jaime Olmo <jamesxv7@gmail.com>
2015-07-27 17:47:10 -0400

    Tuple

--- ejemplos/tipos basicos/tipos-basicos.ts
+++ ejemplos/tipos basicos/tipos-basicos.ts
@@ -16,7 +16,7 @@ var name: string = "Jaime"
 var lastName: string = "Olmo";
 
 // Como concadenar dos strings
-alert(name + " " + lastName)
+//alert(name + " " + lastName)
 
 // Cadena de escape 
 var shortDescription: string = "Doblar a la 'derecha'."
@@ -25,7 +25,7 @@ shortDescription = 'Continuar a la \'izquierda\'.'
 
 // Como concadenar strings y variables tipo number
 shortDescription = "Su edad es " + age + " años.";
-alert(shortDescription);
+//alert(shortDescription);
 
 /* Errores
 shortDescription = "Doblar a la "izquierda".
@@ -34,4 +34,18 @@ shortDescription = "Doblar a la "izquierda".
 // Template String
 var sentence: string = `Hello, my name is ${ name }.
 I'll be ${ age + 1 } years old next month.`
-alert(sentence)
+//alert(sentence)
+
+// Arrays
+
+// Tuple array
+var roomInfo: [number, string];
+var rNum: number;
+var rName: string;
+roomInfo = [101, "Habitacion 1"]; 
+//roomInfo= "102", "Habitacion 2";
+roomInfo.push("201-A","Habitacion 38");
+
+
+//tupleArray =  [201, "Habitacion 38"];
+alert(roomInfo[0] + roomInfo[1]);
